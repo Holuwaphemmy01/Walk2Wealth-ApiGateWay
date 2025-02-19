36761104be0718b9573cb615dd0252b98159021f
Jacob Oluwafemi <jacoboluwafemi2002@gmail.com>
2025-02-19 23:29:02 +0100

    first commit

---
+++ src/test/java/com/walk2wealthgateway/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walk2wealthgateway.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
